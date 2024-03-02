f6013f019ec216c351eddf9d72abfd197e3f5360
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-02 15:05:02 +0100

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Mikhail Chernyshov on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
